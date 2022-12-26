a2bba615207644556ec2fc43381afa23f7300367
Kirill Lahtachev <lahtachev@gmail.com>
2022-12-26 21:09:46 +0100

    time-tool: add msg

---
+++ apps/time-tool/cmd/msg.go
@@ -0,0 +1,12 @@
+package cmd
+
+import (
+	"github.com/fatih/color"
+)
+
+var (
+	Danger  = color.New(color.FgRed)
+	Info    = color.New(color.FgCyan)
+	Warning = color.New(color.FgYellow)
+	Success = color.New(color.FgGreen)
+)
