53acf5e25cf52c24d3b3721acb77b7fb08cd64f5
Kirill Lahtachev <lahtachev@gmail.com>
2022-12-26 21:09:59 +0100

    time-tool: update timestamp command

--- apps/time-tool/cmd/timestamp.go
+++ apps/time-tool/cmd/timestamp.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"time"
 
-	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 )
 
@@ -19,10 +18,9 @@ var TimestampCmd = &cobra.Command{
 
 		jsonStr := []byte(string(reqBody))
 		req := GetReq(Post, "/api/work-time/timestamp", jsonStr)
-		body := GetBody(req)
+		body, _ := GetBody(req)
 		result := TimestampRes{}
 		json.Unmarshal([]byte(body), &result)
-		cyan := color.New(color.FgCyan)
-		cyan.Printf("Timestamp: %s\n", result.Data.Timestamp)
+		Info.Printf("Timestamp: %s\n", result.Data.Timestamp)
 	},
 }
