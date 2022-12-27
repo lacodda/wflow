823bc0faa02cc449fe830f51b205569235596085
Kirill Lahtachev <lahtachev@gmail.com>
2022-12-27 14:18:31 +0100

    time-tool: update login command

--- apps/time-tool/cmd/login.go
+++ apps/time-tool/cmd/login.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"errors"
-	"fmt"
 
 	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
@@ -13,57 +12,46 @@ var LoginCmd = &cobra.Command{
 	Use:   "login",
 	Short: "Authenticate to the FA server using the OAuth Device Flow.",
 	Run: func(cmd *cobra.Command, args []string) {
-		templates := &promptui.PromptTemplates{
-			Prompt:  "{{ . }} ",
-			Valid:   "{{ . | green }} ",
-			Invalid: "{{ . | red }} ",
-			Success: "{{ . | bold }} ",
-		}
-
-		validateLogin := func(input string) error {
-			if len(input) < 3 {
-				return errors.New("Login must have more than 3 characters")
-			}
-			return nil
-		}
-
-		validatePassword := func(input string) error {
-			if len(input) < 0 {
-				return errors.New("Password must have more than 3 characters")
-			}
-			return nil
-		}
-
 		loginPrompt := promptui.Prompt{
-			Label:     "Login",
-			Validate:  validateLogin,
-			Templates: templates,
+			Label:    "Login",
+			Validate: validateLogin,
 		}
 
 		passwordPrompt := promptui.Prompt{
-			Label:     "Password",
-			Validate:  validatePassword,
-			Mask:      '*',
-			Templates: templates,
+			Label:    "Password",
+			Validate: validatePassword,
+			Mask:     '*',
 		}
 
-		login, err := loginPrompt.Run()
-		if err != nil {
-			fmt.Printf("Prompt failed %v\n", err)
-			return
-		}
-		password, err := passwordPrompt.Run()
-		if err != nil {
-			fmt.Printf("Prompt failed %v\n", err)
+		login, errL := loginPrompt.Run()
+		password, errP := passwordPrompt.Run()
+		promptErr := NotNil(errL, errP)
+
+		if promptErr != nil {
+			Danger("Prompt failed: %v\n", promptErr)
 			return
 		}
 
 		token, err := SignIn(login, password)
 		if err != nil {
-			Danger.Printf(err.Error())
+			Danger("Error: %v\n", err.Error())
 			return
 		}
 		SaveToken(token)
-		Success.Printf("You successfully authenticated!")
+		Success("You successfully authenticated!\n")
 	},
+}
+
+func validateLogin(input string) error {
+	if len(input) < 3 {
+		return errors.New("LOGIN MUST HAVE MORE THAN 3 CHARACTERS")
+	}
+	return nil
+}
+
+func validatePassword(input string) error {
+	if len(input) == 0 {
+		return errors.New("PASSWORD MUST HAVE MORE THAN 1 CHARACTERS")
+	}
+	return nil
 }
