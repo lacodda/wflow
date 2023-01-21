292c3132a941fd5c60ab7d182634170141f1abfb
Kirill Lakhtachev <lahtachev@gmail.com>
2023-01-21 16:13:27 +0100

    time-tool: added "MinutesToTimeStr" and "PadLeft" functions to helper

--- apps/time-tool/core/helper.go
+++ apps/time-tool/core/helper.go
@@ -1,5 +1,12 @@
 package core
 
+import (
+	"fmt"
+	"strings"
+)
+
+const padLimit = 8192
+
 func If[T any](cond bool, vtrue, vfalse T) T {
 	if cond {
 		return vtrue
@@ -23,4 +30,37 @@ func Contains[T comparable](s []T, e T) bool {
 		}
 	}
 	return false
+}
+
+func MinutesToTimeStr(totalMinutes int) string {
+	minutes := fmt.Sprintf("%v", totalMinutes%60)
+	hours := fmt.Sprintf("%v", float64(totalMinutes/60))
+
+	return fmt.Sprintf("%v:%v", PadLeft(hours, 2, "0"), PadLeft(minutes, 2, "0"))
+}
+
+func PadLeft(in string, size int, sep string) string {
+	if sep == "" {
+		sep = " "
+	}
+	sepLen := len(sep)
+	inLen := len(in)
+	pads := size - inLen
+	if pads <= 0 {
+		return in
+	}
+	if sepLen == 1 && pads <= padLimit {
+		return strings.Repeat(sep, pads) + in
+	}
+	if pads == sepLen {
+		return sep + in
+	} else if pads < sepLen {
+		return sep[0:pads] + in
+	} else {
+		var padding string
+		for i := 0; i < pads; i++ {
+			padding += string(sep[i%sepLen])
+		}
+		return padding + in
+	}
 }
