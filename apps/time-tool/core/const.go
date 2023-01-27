68dff8cf31db325d886e58f32b35bae0b537cc3f
Kirill Lakhtachev <lahtachev@gmail.com>
2023-01-27 08:46:31 +0100

    time-tool: added common consts

---
+++ apps/time-tool/core/const.go
@@ -0,0 +1,9 @@
+package core
+
+const (
+	DateTpl     string = "2006-01-02"
+	DateDotTpl  string = "02.01.2006"
+	DateFileTpl string = "20060102"
+	DateISOTpl  string = "2006-01-02T15:04:05Z"
+	TimeTpl     string = "15:04"
+)
