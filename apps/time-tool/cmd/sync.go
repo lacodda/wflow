b2ef532187dc50741296edb6b4eec32de229abae
Kirill Lakhtachev <lahtachev@gmail.com>
2023-02-14 15:26:34 +0100

    time-tool: updated sync command

--- apps/time-tool/cmd/sync.go
+++ apps/time-tool/cmd/sync.go
@@ -26,24 +26,37 @@ var SyncCmd = &cobra.Command{
 			printTimestamps(deletedTimestamps)
 			return
 		}
-		timestamps, _ := db.GetTimestamps()
-		if len(timestamps) == 0 {
-			core.Warning("No records for synchronizing with the server!\n")
-			return
-		}
-		err := handleTimestamps(timestamps)
-		if err != nil {
+		if err := syncTimestamps(); err != nil {
 			core.Danger("Error: %v\n", err.Error())
 			return
 		}
-		core.Info("Timestamps:\n")
-		printTimestamps(timestamps)
+		if err := syncTasks(); err != nil {
+			core.Danger("Error: %v\n", err.Error())
+			return
+		}
 
 		if !FlagSyncShow {
 			db.DeleteTimestamps()
+			db.DeleteTasks()
 			core.Success("Your data is synced!\n")
 		}
 	},
+}
+
+func syncTimestamps() error {
+	timestamps, _ := db.GetTimestamps()
+	if len(timestamps) == 0 {
+		core.Warning("No timestamps for synchronizing with the server!\n")
+		return nil
+	}
+	err := handleTimestamps(timestamps)
+	if err != nil {
+		return err
+	}
+	core.Info("Timestamps:\n")
+	printTimestamps(timestamps)
+
+	return nil
 }
 
 func handleTimestamps(timestamps []core.Timestamp) error {
@@ -61,5 +74,44 @@ func handleTimestamps(timestamps []core.Timestamp) error {
 func printTimestamps(timestamps []core.Timestamp) {
 	for _, timestamp := range timestamps {
 		core.Info("[%d] %s (%s)\n", timestamp.Id, timestamp.Timestamp.Format("2006-01-02 15:04"), timestamp.Type)
+	}
+}
+
+func syncTasks() error {
+	tasks, _ := db.GetTasks()
+	if len(tasks) == 0 {
+		core.Warning("No tasks for synchronizing with the server!\n")
+		return nil
+	}
+	err := handleTasks(tasks)
+	if err != nil {
+		return err
+	}
+	core.Info("Tasks:\n")
+	printTasks(tasks)
+
+	return nil
+}
+
+func handleTasks(tasks []core.Task) error {
+	for _, task := range tasks {
+		if !FlagSyncShow {
+			_, err := api.PushTask(core.TaskReq{
+				Date:         task.Date,
+				Name:         task.Name,
+				Comment:      task.Comment,
+				Completeness: task.Completeness,
+			})
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func printTasks(tasks []core.Task) {
+	for _, task := range tasks {
+		core.Info("[%d] %s %s (Completeness: %v%%)\n", task.Id, task.Date.Format("2006-01-02"), task.Name, task.Completeness)
 	}
 }
